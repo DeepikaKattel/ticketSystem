93c673b033dac0dfa825dfc61e55bbbd3bcc5d8b
Deepika <deepika.kattel80@gmail.com>
2020-10-21 12:26:34 +0545

    Changed Font family and logo of frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand mr-auto" href="/"><img src="{{asset('images/jhunLogo.png')}}" height="50" width="100"></a>
+                <a class="navbar-brand mr-auto" href="/"><img src="{{asset('images/jhunLogo.png')}}" height="60" width="120"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
